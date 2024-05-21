b9e867cb1a091cef7a7e6b38e4d13a663da508bd
German <gonzagerman924@gmail.com>
2024-05-21 15:45:11 -0600

    Eliminar y editar funcionales

--- app/src/main/java/ReecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/ReecyclerViewHelpers/Adaptador.kt
@@ -10,10 +10,11 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import Modelo.Conexion
+import android.app.AlertDialog
 
 
 class
-Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapter<ViewHolder>() {
+ Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapter<ViewHolder>() {
 
 
     fun ActualizarLista(nuevalista: List<dataClassMascotas>){
@@ -21,6 +22,38 @@ Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapter<Vie
         Datos = nuevalista
         notifyDataSetChanged()
     }
+
+
+    ///////////////////TODO: ELIMINAR DATOS
+
+    fun eliminarDatos(nombreMascota: String, posicion: Int){
+
+        //Actulizo la lista de datos y notifico el adaptador
+        val listaDatos = Datos.toMutableList()
+        listaDatos.removeAt(posicion)
+
+        GlobalScope.launch (Dispatchers.IO){
+            val objConexion = Conexion().cadenaConexion()
+
+
+            val deleteMascota = objConexion?.prepareStatement(
+                "delete from tbmascotas where nombreMascota = ? ")!!
+            deleteMascota.setString(1, nombreMascota)
+            deleteMascota.executeUpdate()
+
+            val commit = objConexion?.prepareStatement("commit")!!
+            commit.executeUpdate()
+
+        }
+
+Datos = listaDatos.toList()
+        notifyItemRemoved(posicion)
+        notifyDataSetChanged()
+
+
+    }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista =
@@ -33,6 +66,35 @@ Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapter<Vie
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val producto = Datos[position]
         holder.textView.text = producto.nombreMascotas
+
+
+        holder.imgBorrar.setOnClickListener {
+            ///creamos un alert dialog
+
+
+            val context = holder.itemView.context
+
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Eliminar")
+            builder.setMessage("¿Desea eliminar la mascota?")
+
+
+            //Botones
+            builder.setPositiveButton("si"){
+                dialog, which ->
+                eliminarDatos(producto.nombreMascotas, position)
+            }
+            builder.setNegativeButton("No"){
+                dialog, which ->
+                dialog.dismiss()
+
+            }
+
+            val dialog = builder.create()
+            dialog.show()
+
+        }
+
     }
 
 }
